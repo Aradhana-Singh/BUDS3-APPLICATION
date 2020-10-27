35c14e67ed20ea1a577c96f2bdea8a7b6c80d9c5
Aradhana Singh <aradhana971127@gmail.com>
2020-10-27 19:00:16 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { HolderComponent } from './holder/holder.component';
+import { HomeComponent } from './home/home.component';
+import { LibraryComponent } from './library/library.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '' , redirectTo:"/home", pathMatch:"full" },
+  {path: 'home', component:HolderComponent},
+  {path: 'library', component:LibraryComponent},
+  {path: 'contact-us', component:ContactUsComponent},
+  {path: '**', component:PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'; 
+import { MenubarModule } from 'primeng/menubar';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LibraryComponent } from './library/library.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { HolderComponent } from './holder/holder.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component'; 
+import {InputTextModule} from 'primeng/inputtext';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    LibraryComponent,
+    ContactUsComponent,
+    HolderComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MenubarModule,
+    RouterModule,
+    InputTextModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
